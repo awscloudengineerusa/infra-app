7f6fbb6dd2aaf29dbb902b4e6224a969e6a7c514
gitpractitioner <gitpractitioner@aiit.com>
2026-06-22 14:12:07 -0400

    update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "ec2" {
   source = "./modules/ec2"
 
   ami_id        = "ami-0521cb2d60cfbb1a6"
-  instance_type = "t3.micro  "
+  instance_type = "t3.micro"
   instance_name = "terraform-server"
 }
 
